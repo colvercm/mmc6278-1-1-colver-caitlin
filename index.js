05e17693ba2713b611e8edb9c58347b04f635d78
Caitlin Colver <colvercm@gmail.com>
2023-09-03 13:22:06 -0600

    Finally passed all tests

--- index.js
+++ index.js
@@ -11,23 +11,55 @@ program
 program
   .command("getQuote")
   .description("Retrieves a random quote")
-  .action(async () => 
+  .action(async () => {
     // TODO: Pull a random quote from the quotes.txt file
+    try {
+      const quotesData = await fs.readFile(QUOTE_FILE, `utf-8`);
+      const lineBreak = quotesData.split(`\n`);
+        console.log(lineBreak)
+      const randomQuote = lineBreak[Math.floor(Math.random() * lineBreak.length)];
+      const randomQuoteSelection = randomQuote.split(`|`)[0];
     // console log the quote and author
     // You may style the text with chalk as you wish
+        console.log(chalk.yellow.bold(randomQuoteSelection));
+      
+    }catch (err){
+      const error = chalk.red.bold
+      console.log(error('Error!'));
+    }
+});
 
 program
   .command("addQuote <quote> [author]")
   .description("adds a quote to the quote file")
   .action(async (quote, author) => {
+  
     // TODO: Add the quote and author to the quotes.txt file
-    // If no author is provided,
-    // save the author as "Anonymous".
-    // After the quote/author is saved,
-    // alert the user that the quote was added.
-    // You may style the text with chalk as you wish
-    // HINT: You can store both author and quote on the same line using
-    // a separator like pipe | and then using .split() when retrieving
-  });
+    try {
+      // If no author is provided,
+      // save the author as "Anonymous".
+      if (!author) {
+        const anonymous = "Anonymous" ;
+        const unknownAuthor = fs.appendFile(QUOTE_FILE, ["\n" + quote + "|" + anonymous]);
+      // After the quote/author is saved,
+      // alert the user that the quote was added.
+          console.log(`${chalk.yellow(quote)} - ${chalk.white(unknownAuthor)}`);
+          console.log(chalk.magenta('Quote added successfully!'));
+          
+      }else if (author){
+        // If author is provided - saves author's name
+        // alert the user that the quote was added.
+        const knownAuthor = await fs.appendFile(QUOTE_FILE, ["\n" + quote + "|" + author])
+        console.log(`${chalk.yellow(quote)} - ${chalk.white(author)}`);
+        console.log(chalk.magenta('Quote added successfully!'));
+      }
+
+        } catch (err){
+          const error = chalk.red.bold
+          console.log(error('Error!'));
+      }
+    });  
+      
+  
 
 program.parse();
